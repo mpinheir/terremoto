f4b0b7bd0ea8838f656bc1f1ee7ca0d5faecaa93
Marcelo Pinheiro <mpinheir@gmail.com>
2020-07-11 20:54:29 -0700

    Added output and Pylint enhancements.

--- terremoto.py
+++ terremoto.py
@@ -1,15 +1,19 @@
-# Programa em Python para listar os terremotos acima de 6 graus nos últimos 30 dias.
+#---------------------------------------------------------------------------------------
+"""Programa em Python para listar os terremotos acima de 6 graus nos ultimos 30 dias."""
 # Fonte dos dados: https://earthquake.usgs.gov/ - USGS
 # Autor: Marcelo Pinheiro - [Twitter](http://twitter.com/mpinheir)
 #---------------------------------------------------------------------------------------
-import urllib.request, json
+import urllib.request
+import json
 
-with urllib.request.urlopen("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson") as url:
+URLTOACESS = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+with urllib.request.urlopen(URLTOACESS) as url:
     DATA = json.loads(url.read().decode())
     NUMERO_DE_TERREMOTOS = DATA['metadata']['count']
-    print("Terremotos acima de 6 graus nos últimos 30 dias:")
-    for i in range (0, NUMERO_DE_TERREMOTOS-1):
+    print("Terremotos acima de 6 graus nos ultimos 30 dias:")
+    print("================================================")
+    for i in range(0, NUMERO_DE_TERREMOTOS-1):
         Magnitude = DATA['features'][i]['properties']['mag']
         if Magnitude > 6:
             Epicentro = DATA['features'][i]['properties']['place']
-            print(Epicentro,'--Magnitude: ',Magnitude)
+            print(Epicentro, '--> Magnitude:', Magnitude)
